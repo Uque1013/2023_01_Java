65f6dde6f40b137ffdf0b31f371975ba241bfb3f
kye <kye6735@naver.com>
2023-04-11 10:05:13 +0900

    주석 추가

--- Pro02Package/src/pack05/MathEx.java
+++ Pro02Package/src/pack05/MathEx.java
@@ -6,10 +6,10 @@ public class MathEx {
 		double a = 34.567;
 		
 		System.out.println(Math.PI);
-		System.out.println(Math.ceil(a));
-		System.out.println(Math.floor(a));
-		System.out.println(Math.sqrt(9));
-		System.out.println(Math.exp(2));
+		System.out.println(Math.ceil(a)); // 올림
+		System.out.println(Math.floor(a)); // 내림
+		System.out.println(Math.sqrt(9)); // 루트(제곱근)
+		System.out.println(Math.exp(2));  // e(자연대수라고 하는데 2.718282)의 2승
 		System.out.println(Math.round(3.14));
 
 		System.out.println("이번주 행운의 번호는 ");
