25cffcdbad6f3e33027e668d052f274135153544
kye <kye6735@naver.com>
2023-04-11 09:19:26 +0900

    StringTokenzier 클래스 메소드 활용

---
+++ Pro02Package/src/pack05/StringTokenizerEx.java
@@ -0,0 +1,13 @@
+package pack05;
+
+import java.util.StringTokenizer;
+
+public class StringTokenizerEx {
+
+	public static void main(String[] args) {
+		StringTokenizer st = new StringTokenizer("홍길동/장화/홍련/콩쥐/팥쥐","/");
+		while(st.hasMoreTokens())
+			System.out.println(st.nextToken());
+	}
+
+}
