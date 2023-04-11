8b4b69cc079e7425c40e03e8ba4dcc5e7d7eea3e
kye <kye6735@naver.com>
2023-04-11 10:04:31 +0900

    주석 추가

--- Pro02Package/src/pack05/StringTokenizerEx.java
+++ Pro02Package/src/pack05/StringTokenizerEx.java
@@ -6,8 +6,12 @@ public class StringTokenizerEx {
 
 	public static void main(String[] args) {
 		StringTokenizer st = new StringTokenizer("홍길동/장화/홍련/콩쥐/팥쥐","/");
-		while(st.hasMoreTokens())
+		// 구분자는 "/"로 설정하였고, 토큰들은 "홍길동", "장화", "홍련", "콩쥐", "팥쥐"
+		// 그리고 st 첫번째 토큰을 가리킨다
+		while(st.hasMoreTokens())	// st가 가리키는 곳에 토큰이 존재하면 true를 반환
 			System.out.println(st.nextToken());
+		// st.nextToken()은 st가 가리키는 곳의 토큰을 출력한 후에
+		// st는 다음 토큰을 가리키게 된다.
 	}
 
 }
