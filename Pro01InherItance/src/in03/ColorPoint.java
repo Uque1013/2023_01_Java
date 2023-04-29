d17944c10be2603a4c597256aea08ee92fb9c944
kye <kye6735@naver.com>
2023-04-29 15:14:34 +0900

    예제 1-3 super() 활용 복습

--- Pro01InherItance/src/in03/ColorPoint.java
+++ Pro01InherItance/src/in03/ColorPoint.java
@@ -1,18 +1,14 @@
 package in03;
 
-public class ColorPoint extends Point {
-	private String color; // 점의 색
+public class ColorPoint extends  Point{
+	private String color;	// 점의 색상
 	
 	public ColorPoint(int x, int y, String color) {
-		super(x,y); // x=5, y=6
-		// super(); super()는 무조건 하나면 올 수 있음
-		this.color = color; //멤버변수에 color값을 저장
-		// super()는 무조건 맨 위, 첫 번째 라인에 위치 
+		super(x, y);		// super()은 무조건 하나만
+		this.color = color;
 	}
-
 	public void showColorPoint() {
 		System.out.print(color);
-		showPoint();
+		showPoint();		// Point의 showPoint() 호출
 	}
-	
 }
--- Pro01InherItance/src/in03/Point.java
+++ Pro01InherItance/src/in03/Point.java
@@ -1,20 +1,15 @@
 package in03;
 
 public class Point {
-	private int x, y; // 한 점을 구성하는 x, y 좌표 
+	private int x, y;
 	
-	public Point() { // 기본생성자
-		this.x=0;
-		this.y=0;
+	public Point() {		// 기본 생성자
+		this.x = 0; this.y = 0;
 	}
-	
-	public Point(int x, int y) { // 정수형 매개변수 1개를 가지고 있는 생성자
-		this.x=x;
-		this.y=y;
-	} // 멤버변수 x=5, y=6
-	
-	public void showPoint() { // 점의 좌표를 출력
+	public Point(int x, int y) {	// 매개변수 생성자
+		this.x = x; this.y = y;
+	}
+	public void showPoint() {		// 점의 좌표 출력
 		System.out.println("("+x+","+y+")");
 	}
-	
 }
