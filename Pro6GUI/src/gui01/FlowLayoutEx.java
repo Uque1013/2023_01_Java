11572b766b9cd1a97a2ef4a33a038dbe8658ae4c
kye <kye6735@naver.com>
2023-08-01 09:08:20 +0900

    예제 9-3 FLowLayout 배치관리자 활용

---
+++ Pro6GUI/src/gui01/FlowLayoutEx.java
@@ -0,0 +1,28 @@
+package gui01;
+import javax.swing.*;
+import java.awt.*;
+
+public class FlowLayoutEx extends JFrame{
+	
+	public FlowLayoutEx() {
+		setTitle("FlowLayout Sample");
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		Container c = getContentPane();
+		
+		c.setLayout(new FlowLayout(FlowLayout.LEFT, 30, 40));
+		c.add(new JButton("add"));
+		c.add(new JButton("sub"));
+		c.add(new JButton("mul"));
+		c.add(new JButton("div"));
+		c.add(new JButton("Calculate"));
+		
+		setSize(300, 200);
+		setVisible(true);
+	}
+
+	public static void main(String[] args) {
+		new FlowLayoutEx();
+
+	}
+
+}
